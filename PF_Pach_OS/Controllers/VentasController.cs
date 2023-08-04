2aa6c07bd5055a5d2205f86c8b4c98bf4339db40
Miguelito250 <mjackmeca@gmail.com>
2023-08-04 02:41:19 -0500

    Agregar y remover lista de detalles
    Sin estilos ni funcionamiento

---
+++ PF_Pach_OS/Controllers/VentasController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PF_Pach_OS.Models;
+
+namespace PF_Pach_OS.Controllers
+{
+
+    public class VentasController : Controller
+    {
+        private readonly Pach_OSContext _context;
+
+        public VentasController(Pach_OSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ventas
+        public async Task<IActionResult> Index()
+        {
+            var pach_OSContext = _context.Ventas.Include(v => v.IdEmpleadoNavigation);
+            return View(await pach_OSContext.ToListAsync());
+        }
+
+        // GET: Ventas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Ventas == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas
+                .Include(v => v.IdEmpleadoNavigation)
+                .FirstOrDefaultAsync(m => m.IdVenta == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        // GET: Ventas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdEmpleado"] = new SelectList(_context.Empleados, "IdEmpleado", "IdEmpleado");
+            return View();
+        }
+
+        // POST: Ventas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdVenta,FechaVenta,TotalVenta,TipoPago,Pago,PagoDomicilio,IdEmpleado")] Venta venta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEmpleado"] = new SelectList(_context.Empleados, "IdEmpleado", "IdEmpleado", venta.IdEmpleado);
+            return View(venta);
+        }
+
+        // GET: Ventas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Ventas == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas.FindAsync(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdEmpleado"] = new SelectList(_context.Empleados, "IdEmpleado", "IdEmpleado", venta.IdEmpleado);
+            return View(venta);
+        }
+
+        // POST: Ventas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdVenta,FechaVenta,TotalVenta,TipoPago,Pago,PagoDomicilio,IdEmpleado")] Venta venta)
+        {
+            if (id != venta.IdVenta)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VentaExists(venta.IdVenta))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEmpleado"] = new SelectList(_context.Empleados, "IdEmpleado", "IdEmpleado", venta.IdEmpleado);
+            return View(venta);
+        }
+
+        // GET: Ventas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Ventas == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas
+                .Include(v => v.IdEmpleadoNavigation)
+                .FirstOrDefaultAsync(m => m.IdVenta == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        // POST: Ventas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Ventas == null)
+            {
+                return Problem("Entity set 'Pach_OSContext.Ventas'  is null.");
+            }
+            var venta = await _context.Ventas.FindAsync(id);
+            if (venta != null)
+            {
+                _context.Ventas.Remove(venta);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VentaExists(int id)
+        {
+          return (_context.Ventas?.Any(e => e.IdVenta == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PF_Pach_OS/wwwroot/js/Ventas.js
@@ -0,0 +1,24 @@
+﻿agregarProducto()
+
+function agregarProducto() {
+    var listaProductos = document.getElementById("lista-productos")
+
+    var nuevoProducto = document.createElement("div");
+    nuevoProducto.classList.add("lista-detalles");
+
+    nuevoProducto.innerHTML = "<select>Categoria</select><select>Producto</select><input type='number' class='formularios'><label>Precio</label>";
+
+    listaProductos.appendChild(nuevoProducto);
+}
+
+function removerProducto() {
+    var listaProductos = document.getElementById("lista-productos")
+    var listaDetalles = document.getElementsByClassName("lista-detalles");
+
+    if (listaDetalles.length > 1) {
+        var ultimoDetalle = listaDetalles[listaDetalles.length - 1];
+
+        listaProductos.removeChild(ultimoDetalle)
+    }
+    
+}
