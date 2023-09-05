20b6e6ee756903928ffbae7b38586fbd62b0e3da
Miguelito250 <mjackmeca@gmail.com>
2023-09-05 02:08:33 -0500

    Diseño de Login

--- PF_Pach_OS/Models/Pach_OSContext.cs
+++ PF_Pach_OS/Models/Pach_OSContext.cs
@@ -33,7 +33,6 @@ namespace PF_Pach_OS.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=localhost;Initial Catalog=Pach_OS;integrated security=True; TrustServerCertificate=True");
             }
         }
--- PF_Pach_OS/Program.cs
+++ PF_Pach_OS/Program.cs
@@ -26,12 +26,29 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
-
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    if (!context.User.Identity.IsAuthenticated)
+    {
+        
+        if (context.Request.Path != "/Identity/Account/Login")
+        {
+            context.Response.Redirect("/Identity/Account/Login");
+            return;
+        }
+    }
+
+    // Si el usuario está autenticado o la solicitud está en la página de Login o Registro,
+    // continuar con el siguiente middleware
+    await next.Invoke();
+});
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
