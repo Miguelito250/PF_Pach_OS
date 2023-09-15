804b9215ec6804f1e550a7c0e7c60478c977e20b
navarro243 <125632023+Miguelito250@users.noreply.github.com>
2023-09-15 15:46:18 -0500

    Diseño registro usuario

--- PF_Pach_OS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PF_Pach_OS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,17 +74,17 @@ namespace PF_Pach_OS.Areas.Identity.Pages.Account
 
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "Correo")]
             public string Email { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Contraseña")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Confirmar contraseña")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
