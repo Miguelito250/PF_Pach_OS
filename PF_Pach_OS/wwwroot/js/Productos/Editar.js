1f6c4dd81700eda0b48cf2ec04f0b054b0cce5f2
navarro243 <juanandresnavarroherrera@gmail.com>
2023-10-18 01:46:36 -0500

    Productos V05.4.7

--- PF_Pach_OS/wwwroot/js/Productos/Editar.js
+++ PF_Pach_OS/wwwroot/js/Productos/Editar.js
@@ -71,7 +71,7 @@
         console.log(NomProducto)
         if (contarNombre == 0) {
             validacion = true;
-            mostrarError('Por favor ingrese un nombre al producto.');
+            mostrarError('Por favor ingrese un nombre al producto.', '#Producto_NomProducto');
         } else if (contarNombre < minCaracteres) {
             validacion = true;
             mostrarError('El nombre del producto debe tener al menos 4 caracteres.', '#Producto_NomProducto');
@@ -181,9 +181,9 @@
 (function () {
     'use strict';
     window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
         var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
+
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
@@ -307,7 +307,7 @@ function ocultarFila(button) {
     } else {
         Agregar_Eliminar(IdREceta)
     }
-    fila.style.display = 'none';
+    fila.remove();
 }
 function Agregar_Eliminar(idreceta) {
     Eliminar_Receta.push(idreceta);
@@ -331,3 +331,10 @@ function Atualizar_Recetas(objetoJSON) {
     Recetas_Actualizadas.push(objetoJSON);
     console.log(Recetas_Actualizadas);
 }
+
+ocument.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("formulario1").addEventListener("submit", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+});
