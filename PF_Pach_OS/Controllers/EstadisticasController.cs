138217afedbeef3fedcb1757ff014eb658ca5178
Karen Lorena <137014022+Karenlorena122127@users.noreply.github.com>
2023-10-27 23:58:41 -0500

    graficas con datos de ventas y detalles

---
+++ PF_Pach_OS/Controllers/EstadisticasController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PF_Pach_OS.Models;
+using System.Diagnostics.Metrics;
+
+namespace PF_Pach_OS.Controllers { 
+
+    public class EstadisticasController : Controller
+{
+    private readonly Pach_OSContext _context;
+    public EstadisticasController(Pach_OSContext context)
+    {
+        _context = context;
+    }
+        [HttpGet]
+        public IActionResult ObtenerVentasMensuales()
+        {
+            // Obtener el mes actual y el año actual
+            int mesActual = DateTime.Now.Month;
+            int añoActual = DateTime.Now.Year;
+
+            // Crear una lista para almacenar los datos de ventas mensuales
+            List<decimal> ventasMensuales = new List<decimal>();
+
+            for (int mes = 1; mes <= mesActual; mes++)
+            {
+                // Calcular el primer día del mes actual
+                DateTime primerDiaDelMes = new DateTime(añoActual, mes, 1);
+
+                // Calcular el último día del mes actual
+                DateTime ultimoDiaDelMes = new DateTime(añoActual, mes, DateTime.DaysInMonth(añoActual, mes));
+
+                // Calcular el total de ventas para el mes actual
+                decimal totalVentasMes = _context.Ventas
+                    .Where(v => v.FechaVenta >= primerDiaDelMes && v.FechaVenta <= ultimoDiaDelMes)
+                    .Sum(v => v.TotalVenta.GetValueOrDefault());
+
+                ventasMensuales.Add(totalVentasMes);
+            }
+
+            return Json(ventasMensuales);
+        }
+
+        public IActionResult ObtenerTotalVentasAnuales()
+        {
+            // Obtener el año actual
+            int añoActual = DateTime.Now.Year;
+
+            // Calcular el primer día del año actual
+            DateTime primerDiaDelAño = new DateTime(añoActual, 1, 1);
+
+            // Calcular el último día del año actual
+            DateTime ultimoDiaDelAño = new DateTime(añoActual, 12, 31);
+
+            // Calcular el total de ventas para todo el año
+            decimal totalVentasAnuales = _context.Ventas
+                .Where(v => v.FechaVenta >= primerDiaDelAño && v.FechaVenta <= ultimoDiaDelAño)
+                .Sum(v => v.TotalVenta.GetValueOrDefault());
+
+            return Json(totalVentasAnuales);
+        }
+        public IActionResult ObtenerDiferenciaVentasMesAnterior()
+        {
+            // Obtener el primer día del mes actual
+            DateTime primerDiaDelMesActual = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+            // Calcular el último día del mes anterior
+            DateTime ultimoDiaDelMesAnterior = primerDiaDelMesActual.AddDays(-1);
+
+            // Calcular el primer día del mes anterior
+            DateTime primerDiaDelMesAnterior = new DateTime(ultimoDiaDelMesAnterior.Year, ultimoDiaDelMesAnterior.Month, 1);
+
+            // Calcular el total de ventas para el mes actual
+            decimal totalVentasMesActual = _context.Ventas
+                .Where(v => v.FechaVenta >= primerDiaDelMesActual && v.FechaVenta <= DateTime.Now)
+                .Sum(v => v.TotalVenta.GetValueOrDefault());
+
+            // Calcular el total de ventas para el mes anterior
+            decimal totalVentasMesAnterior = _context.Ventas
+                .Where(v => v.FechaVenta >= primerDiaDelMesAnterior && v.FechaVenta <= ultimoDiaDelMesAnterior)
+                .Sum(v => v.TotalVenta.GetValueOrDefault());
+
+            // Calcular la diferencia
+            decimal diferencia = totalVentasMesActual - totalVentasMesAnterior;
+
+            // Determinar si es un aumento o una disminución
+            string aumentoODisminucion = diferencia > 0 ? "aumento" : (diferencia < 0 ? "disminución" : "sin cambios");
+
+            return Json(new { diferencia, aumentoODisminucion });
+        }
+
+        public IActionResult ObtenerVentasPorDia(string fecha)
+        {
+            // Convierte la fecha seleccionada a DateTime si es válido
+            if (DateTime.TryParse(fecha, out DateTime fechaSeleccionada))
+            {
+                // Consulta para obtener las ventas del día seleccionado
+                decimal ventasDelDia = _context.Ventas
+                    .Where(v => v.FechaVenta.HasValue && v.FechaVenta.Value.Date == fechaSeleccionada.Date)
+                    .Sum(v => v.TotalVenta.GetValueOrDefault());
+
+                return Json(ventasDelDia);
+            }
+
+            // Maneja el caso en el que la conversión de la fecha falla
+            return Json(0); // Puedes elegir cómo manejar este caso de acuerdo a tus necesidades
+        }
+        public IActionResult ObtenerProductosMasYMenosVendidos()
+        {
+            // Realiza la consulta para obtener los productos más vendidos y menos vendidos
+            var productosMasVendidos = _context.DetalleVentas
+                .GroupBy(dv => dv.IdProducto)
+                .OrderByDescending(g => g.Sum(dv => dv.CantVendida))
+                .Select(g => new { ProductoId = g.Key, TotalVendido = g.Sum(dv => dv.CantVendida) })
+                .FirstOrDefault();
+
+            var productosMenosVendidos = _context.DetalleVentas
+                .GroupBy(dv => dv.IdProducto)
+                .OrderBy(g => g.Sum(dv => dv.CantVendida))
+                .Select(g => new { ProductoId = g.Key, TotalVendido = g.Sum(dv => dv.CantVendida) })
+                .FirstOrDefault();
+
+            // Puedes adaptar este código según la estructura de tu base de datos y tus modelos.
+
+            return Json(new { MasVendido = productosMasVendidos, MenosVendido = productosMenosVendidos });
+        }
+
+
+        private List<Venta> ObtenerDatosVentas()
+        {
+
+            DateTime fechaInicio = DateTime.Now.AddMonths(-1); 
+            DateTime fechaFin = DateTime.Now; 
+
+            List<Venta> datosVentas = _context.Ventas
+
+                .Where(v => v.FechaVenta >= fechaInicio && v.FechaVenta <= fechaFin)
+                .Select(v => new Venta
+                {
+                    IdVenta = v.IdVenta,
+                    FechaVenta = v.FechaVenta,
+                    TotalVenta = v.TotalVenta
+                })
+                .ToList();
+
+            return datosVentas;
+        }
+
+
+    }
+
+
+
+
+}
+
+
--- PF_Pach_OS/wwwroot/js/Informe/Informe.js
+++ PF_Pach_OS/wwwroot/js/Informe/Informe.js
@@ -1,38 +1,123 @@
+﻿
+var obtenerTotalVentasUrl = '@Url.Action("ObtenerTotalVentas", "EstadisticasController")';
+    $(document).ready(function () {
+        $.ajax({
+            url: '/Estadisticas/ObtenerVentasMensuales',
+            type: 'GET',
+            dataType: 'json',
+            success: function (ventasMensuales) {
+                console.log('Datos de ventas mensuales:', ventasMensuales);
 
+                var labels = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+                var datosVentas = {
+                    labels: labels.slice(0, ventasMensuales.length),
+                    datasets: [{
+                        label: 'Ventas',
+                        data: ventasMensuales,
+                        backgroundColor: '#007bff',
+                        borderColor: '#007bff',
+                        borderWidth: 1
+                    }]
+                };
 
-    var salesData = {
-        labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-    datasets: [
-    {
-        label: "Ventas",
-            backgroundColor: "#007bff",
-    borderColor: "rgba(75, 192, 192, 1)",
-            data: [65.000, 59.000, 80.000, 81.000, 56.000, 55.000, 85.000, 15.000, 105.000, 55.000, 25.000, 75.000],
+                var ctx = document.getElementById('sales-chart').getContext('2d');
+                var salesChart = new Chart(ctx, {
+                    type: 'line',
+                    data: datosVentas,
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false
+                    }
+                });
             },
-    ],
-    };
-
-    var ctx = document.getElementById("sales-chart").getContext("2d");
-
-
-    var salesChart = new Chart(ctx, {
-        type: "bar",
-    data: salesData,
-    options: {
-        scales: {
-        y: {
-        beginAtZero: true,
-                },
-            },
-        },
+            error: function () {
+                alert('Error al cargar los datos de ventas mensuales.');
+            }
+        });
     });
 
+$(document).ready(function () {
+    $.ajax({
+        url: '/Estadisticas/ObtenerTotalVentasAnuales',
+        type: 'GET',
+        dataType: 'json',
+        success: function (totalVentasAnuales) {
+            console.log('Total de ventas anuales:', totalVentasAnuales);
 
-    function updateChartData() {
 
-        salesChart.update();
-    }
+            $('#totalVentasAnuales').text('$' + totalVentasAnuales.toFixed(2)); 
+        },
+        error: function () {
+            alert('Error al cargar el total de ventas anuales.');
+        }
+    });
+});
 
+$(document).ready(function () {
+    $.ajax({
+        url: '/Estadisticas/ObtenerDiferenciaVentasMesAnterior',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log('Diferencia de ventas desde el mes anterior:', data.diferencia);
 
-    setInterval(updateChartData, 5000);
+            var diferenciaSpan = $('#diferenciaVentasMesAnterior');
 
+            if (data.aumentoODisminucion === "aumento") {
+                diferenciaSpan.text(data.diferencia.toFixed(2) + ' (↑ Aumento)');
+            } else if (data.aumentoODisminucion === "disminución") {
+                diferenciaSpan.text(data.diferencia.toFixed(2) + ' (↓ Disminución)');
+            } else {
+                diferenciaSpan.text(data.diferencia.toFixed(2) + ' (Sin cambios)');
+            }
+        },
+        error: function () {
+            alert('Error al cargar la diferencia de ventas desde el mes anterior.');
+        }
+    });
+});
+$(document).ready(function () {
+    // Escucha el evento de cambio en el campo de fecha
+    $("#fechaSeleccionada").change(function () {
+        var fechaSeleccionada = $(this).val();
+
+        // Realiza una solicitud AJAX para cargar las ventas del día seleccionado
+        $.ajax({
+            url: '/Estadisticas/ObtenerVentasPorDia',
+            type: 'GET',
+            data: { fecha: fechaSeleccionada },
+            dataType: 'json',
+            success: function (ventasDelDia) {
+                console.log(ventasDelDia)
+                // Muestra las ventas en la vista, por ejemplo, en un div.
+                $('#ventasDelDia').html('Ventas para ' + fechaSeleccionada + ': $' + ventasDelDia.toFixed(2));
+            },
+            error: function () {
+                alert('Error al cargar las ventas para el día seleccionado.');
+            }
+        });
+    });
+});
+$(document).ready(function () {
+    $.ajax({
+        url: '/Estadisticas/ObtenerProductosMasYMenosVendidos',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var ctx = document.getElementById('productosMasYMenosVendidos').getContext('2d');
+            var chart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Más Vendido', 'Menos Vendido'],
+                    datasets: [{
+                        data: [data.MasVendido.TotalVendido, data.MenosVendido.TotalVendido],
+                        backgroundColor: ['rgba(54, 162, 235, 0.6)', 'rgba(255, 99, 132, 0.6)']
+                    }]
+                }
+            });
+        },
+        error: function () {
+            alert('Error al cargar los datos de productos más y menos vendidos.');
+        }
+    });
+});
