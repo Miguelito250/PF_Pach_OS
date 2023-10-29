14408efdcdaf4df7c183a8320d0a6ff5cd0570ce
Miguelito250 <mjackmeca@gmail.com>
2023-10-29 17:51:34 -0500

    Diseño responsivo de Registrar e index

---
+++ PF_Pach_OS/wwwroot/js/Usuarios/Index.js
@@ -0,0 +1,30 @@
+﻿$('#searchInput').on('input', function () {
+    var searchText = $(this).val().trim().toLowerCase();
+
+    // Filtrar las filas de la tabla que coincidan con la búsqueda
+    $('tbody tr').each(function () {
+        var fecha = $(this).find('td:eq(0)').text().trim().toLowerCase();
+        var metodoPago = $(this).find('td:eq(1)').text().trim().toLowerCase();
+        var total = $(this).find('td:eq(2)').text().trim().toLowerCase();
+
+        if (
+            fecha.includes(searchText) ||
+            metodoPago.includes(searchText) ||
+            total.includes(searchText)
+        ) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+});
+// Restablecer el placeholder cuando se borre el contenido o se pierda el foco
+searchInput.on('blur', function () {
+    if ($(this).val().trim() === '') {
+        $(this).addClass('empty');
+    }
+});
+
+searchInput.on('focus', function () {
+    $(this).removeClass('empty');
+});
