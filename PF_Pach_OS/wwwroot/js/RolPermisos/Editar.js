6d3374519b131e4379da255580bfefcdb26376de
navarro243 <juanandresnavarroherrera@gmail.com>
2023-10-29 18:39:13 -0500

    Editar, deshabilitar y estado alestas

--- PF_Pach_OS/wwwroot/js/RolPermisos/Editar.js
+++ PF_Pach_OS/wwwroot/js/RolPermisos/Editar.js
@@ -1,36 +1,16 @@
-﻿var Permisos = [];
-var nomRol;
-var idrol;
+﻿
 
 $(document).ready(function () {
-    var mensaje_permiso = document.getElementById("mensaje_permiso");
 
-    idrol = $("#idrol").val();
+    const formularo_Permiso = document.getElementById('formulario1');
+
+    const nombreRol = document.getElementById('nomrol');
+
+    const Mensaje_nombre = document.getElementById('mensaje_nombre');
+
+    const idRol = document.getElementById('idrol');
 
 
-    $("#btnEnviar").on("click", function () {
-        $('input[name="ValoresSeleccionados"]:checked').each(function () {
-            Permisos.push($(this).val());
-            mensaje_permiso.textContent = ''
-        });
-        console.log(idrol);
-        console.log(nomRol)
-        console.log(Permisos)
-
-    });
-
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    var formulario_rol = document.getElementById("formulario1");
-
-    var nombreRol = document.getElementById("nombreRol");
-    var mensaje_nombre = document.getElementById("mensaje_nombre");
-
-    var mensaje_permiso = document.getElementById("mensaje_permiso");
-
-    const enlacesMenu = document.querySelectorAll('.links-modulos');
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -43,72 +23,93 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    //Escuchadores para rol
-    formulario_rol.addEventListener('submit', EnvioRol);
-    nombreRol.addEventListener('input', ValidarNombre);
-
     function ValidarNombre() {
-        var ValorNombre = nombreRol.value;
-        var caracterMinimo = 3;
-        var caracterMAximo = 20;
 
+
+
+        let ValorNombre = nombreRol.value;
         nombreRol.classList.remove('is-invalid', 'is-valid');
-        mensaje_nombre.textContent = ''
-
+        Mensaje_nombre.textContent = ''
         if (ValorNombre.trim() === '') {
             nombreRol.classList.add('is-invalid');
-            mensaje_nombre.textContent = 'El campo no puede estar vacio';
-        } else if (ValidarNombre < caracterMinimo) {
-            nombreRol.classList.add('is-invalid');
-            mensaje_nombre.textContent = 'El nombre debe tener más de 3 caracteres';
-        } else if (ValidarNombre > caracterMAximo) {
-            nombreRol.classList.add('is-invalid');
-            mensaje_nombre.textContent = 'El nombre debe tener menos de 30 caracteres';
-        } else {
-            nombreRol.classList.add('is-valid');
-            nomRol = ValorNombre;
-        }
+            Mensaje_nombre.textContent = 'El campo no puede estar vacio'
 
+        } else if (ValorNombre.length < 4) {
+            nombreRol.classList.add('is-invalid');
+            Mensaje_nombre.textContent = 'El nombre debe tener almenos 4 caracteres';
+
+        } else if (ValorNombre.length > 20) {
+            nombreRol.classList.add('is-invalid');
+            Mensaje_nombre.textContent = 'El nombre debe tener menos de 20 caracteres';
+        } else {
+            nombreRol.classList.add('is-valid')
+        }
     }
-    function EnvioRol(event) {
+
+    nombreRol.addEventListener('input', ValidarNombre)
+
+    formularo_Permiso.addEventListener('submit', ValidarFormulario)
+
+    var permisos = [];
+    $('input[name="ValoresSeleccionados"]:checked').each(function () {
+        permisos.push($(this).val());
+    });
+
+    function ValidarFormulario(event) {
         event.preventDefault();
         ValidarNombre();
-        if (Permisos.length < 1) {
-            mensaje_permiso.textContent = 'Debe agrgar almenos un permiso'
-        } else {
-            if (formulario_producto.checkValidity()) {
+        var permisos = [];
+        $('input[name="ValoresSeleccionados"]:checked').each(function () {
+            permisos.push($(this).val());
+        });
+        if (formularo_Permiso.checkValidity() && !nombreRol.classList.contains('is-invalid')) {
+            if (permisos.length < 1) {
+                Swal.fire({
+                    title: 'Ups...',
+                    timer: 2700,
+                    text: 'Debes agregar al menos un permiso para Actualizar un rol.',
+                    icon: 'error',
+                    showConfirmButton: false,
+                });
+
+                // Detener la ejecución del código o realizar alguna acción adicional si es necesario
+                return;
+            } else {
                 Swal.fire({
                     title: '¡Éxito!',
-                    text: 'Rol Registrado Correctamente',
+                    text: 'El rol ha sido actualizado',
                     timer: 2400,
                     icon: 'success',
                     showConfirmButton: false,
                 }).then((result) => {
+                    console.log('Formulario válido');
                     $.ajax({
                         url: '/RolPermisos/Editar',
                         type: 'POST',
                         data: {
-                            id: idrol,
-                            permisos: Permisos,
-                            nomRol: nomRol
+                            id: idRol.value,
+                            permisos: permisos,
+                            nomRol: nombreRol.value
                         },
-
                         success: function (data) {
-                            console.log(id)
-                            console.log(Permisos)
-                            console.log(nomRol)
 
                         },
                         error: function (xhr, status, error) {
-                            console.log("Error en la solicitud: " + error);
+                            reject(error);
                         }
                     });
-                    formulario_rol.addEventListener('submit', EnvioRol);
-                    formulario_rol.submit();
+
+                    formularo_Permiso.removeEventListener('submit', ValidarFormulario);
+                    formularo_Permiso.submit();
                 });
             }
-
+        } else {
+            Toast.fire({
+                icon: 'error',
+                title: 'Formulario inválido'
+            });
+            ValidarNombre();
         }
-    }
 
+    }
 });
--- PF_Pach_OS/wwwroot/js/RolPermisos/Index.js
+++ PF_Pach_OS/wwwroot/js/RolPermisos/Index.js
@@ -1,30 +1,90 @@
-﻿var recargar = true;
-$(document).on("click", "#Crear-rol", function () {
+﻿document.addEventListener('DOMContentLoaded', function () {
 
-    var modalBody = $("#modal-body");
-    var url = "/RolPermisos/Crear";
-    modalBody.empty();
-    modalBody.load(url);
-    recargar = false;
+    var recargar = true;
+    $(document).on("click", "#Crear-rol", function () {
 
-
-});
-$(document).on("click", "#Editar-rol", function () {
-
-    var modalBody = $("#modal-body");
-
-    var idRol = $(this).data("id");
-    var url = "/RolPermisos/Editar?id=" + idRol;
-    modalBody.empty();
-    modalBody.load(url);
-    recargar = false;
+        var modalBody = $("#modal-body");
+        var url = "/RolPermisos/Crear";
+        modalBody.empty();
+        modalBody.load(url);
+        recargar = false;
 
 
-});
-if (!recargar) {
-    for (var i = 0; i < 2; i++) {
-        window.location.reload();
+    });
+    $(document).on("click", "#Editar-rol", function () {
+
+        var modalBody = $("#modal-body");
+
+        var idRol = $(this).data("id");
+        var url = "/RolPermisos/Editar?id=" + idRol;
+        modalBody.empty();
+        modalBody.load(url);
+        recargar = false;
+
+
+    });
+    if (!recargar) {
+        for (var i = 0; i < 2; i++) {
+            window.location.reload();
+        }
     }
-}
+
+    window.addEventListener('load', function () {
+        const deshabilitar = document.querySelector('.deshabilitar');
+       
+
+
+
+
+        deshabilitar.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log('Clic detectado en la etiqueta a');
+
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: 'Si desactivas este rol se desactivaran todos los usuarios asocidos a este rol',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, desactivar',
+                cancelButtonText: 'No, volver'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Si el usuario confirma, redirigir a la función "Delete" en el controlador de ventas
+                    window.location.href = deshabilitar.getAttribute('href');
+                }
+            });
+        });
+
+        const habilitar = document.querySelector('.habilitar');
+
+
+        habilitar.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log('Clic detectado en la etiqueta a');
+
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: 'Si activa este rol se ativaran todos los usuarios asocidos a este rol',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, activar',
+                cancelButtonText: 'No'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Si el usuario confirma, redirigir a la función "Delete" en el controlador de ventas
+                    window.location.href = habilitar.getAttribute('href');
+                }
+            });
+        });
+
+    });
+    
+    
+});
+
 
 
